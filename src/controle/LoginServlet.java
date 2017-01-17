fcf645294fc7e59ae907ac4bbbae54d475b77ad5
Sam <samy.mechedddal@gmail.com>
2017-01-17 12:06:42 -0500

    TEST5

--- src/controle/LoginServlet.java
+++ src/controle/LoginServlet.java
@@ -25,7 +25,6 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 	}
 
 	/**
