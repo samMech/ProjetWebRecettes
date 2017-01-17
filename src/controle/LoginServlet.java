001553182baad9e52e77c348f224cb5ccc23ada2
Marc-André <kohakuma4m@gmail.com>
2017-01-17 12:14:42 -0500

    test username 1

--- src/controle/LoginServlet.java
+++ src/controle/LoginServlet.java
@@ -25,6 +25,7 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 	}
 
 	/**
